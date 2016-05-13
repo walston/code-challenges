bdb6f5db19b705fd97251b089c8824c0793afbb5
treezrppl2 <gojiradestroy@gmail.com>
2016-05-13 16:24:23 -0700

    Add more test cases

--- arrays.js
+++ arrays.js
@@ -1,1 +1,15 @@
 module.exports.simple = [-1, 3, -4, 5, 1, -6, 2, 1];
+module.exports.obvious = [1, 0, 1];
+module.exports.empty = [];
+module.exports.none = [1, 2, 3, 4, 5, 6];
+module.exports.huge = (function() {
+  var top = 50000;
+  var x = [];
+  var i = 0;
+  while (i < top) {
+    var k = Math.floor((Math.random() * 10) - 20);
+    x.push(k);
+    i++
+  }
+  return x.concat([0], x);
+})();
