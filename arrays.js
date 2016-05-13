1906b10c601679873c8d8361920847c8e1680d43
treezrppl2 <gojiradestroy@gmail.com>
2016-05-13 16:47:55 -0700

    added a couple new test arrays

--- arrays.js
+++ arrays.js
@@ -1,8 +1,12 @@
 module.exports.simple = [-1, 3, -4, 5, 1, -6, 2, 1];
-module.exports.obvious = [1, 0, 1];
+  // ANSWER: 1
+module.exports.obvious = [1, 1, 0, 2];
+  // ANSWER: 1
 module.exports.empty = [];
+  // ANSWER: -1
 module.exports.none = [1, 2, 3, 4, 5, 6];
-module.exports.huge = (function() {
+  // ANSWER: -1
+module.exports.large = (function() {
   var top = 50000;
   var x = [];
   var i = 0;
@@ -13,3 +17,18 @@ module.exports.huge = (function() {
   }
   return x.concat([0], x);
 })();
+  // ANSWER: 50000
+module.exports.right_weighted = [1, 2, 3, 4, 0, 10];
+  // ANSWER: 4
+  module.exports.huge = (function() {
+    var top = 10000000;
+    var x = [];
+    var i = 0;
+    while (i < top) {
+      var k = Math.floor((Math.random() * 10) - 20);
+      x.push(k);
+      i++
+    }
+    return x.concat([0], x);
+  })();
+    // ANSWER: 10000000
