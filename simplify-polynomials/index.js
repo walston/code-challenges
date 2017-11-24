73bae021df6c5274084c7a6753232b388686a249
Nathan Walston <nathanielwalston@gmail.com>
2017-11-24 08:22:33 -0800

    I had done this wrong before, multiplied variables are only equivalent to the same group of vars

--- simplify-polynomials/index.js
+++ simplify-polynomials/index.js
@@ -2,60 +2,27 @@ function simplify(poly){
   console.log('poly', poly);
 
   var map = {};
-  var multiplier = 1;
-  var nega = false
-  for (var i = 0; i < poly.length; i++) {
-    c = poly[i];
-    if (c === '-') {
-      nega = true;
-      multiplier = 1;
+  var termDef = '([\+\-]?)([0-9]*)([a-z]+)';
+  var terms = poly.match(new RegExp(termDef, 'g'));
+  console.log(terms);
+  for (var i = 0; i < terms.length; i++) {
+    // skip empty strings if (!term[i]) { continue; }
 
-    } else if (c === '+') {
-      nega = false;
-      multiplier = 1;
+    var term = terms[i].match(new RegExp(termDef));
+    console.log('term', term);
+    var s = term[1] === '-' ? '-' : '+';
+    var k = term[2] === '' ? 1 : term[2];
+    // sort in lexicographical order
+    var x = term[3].split('').sort().join('');
 
-    } else if (c.match(/\d/)) {
-      multiplier = multiplier * 10 + parseInt(c, 10);
-
-    } else if (c.match(/\w/)) {
-      if (map[c] !== undefined) {
-        map[c] += multiplier * (nega ? -1 : 1);
-      } else {
-        map[c] = multiplier * (nega ? -1 : 1);
-      }
-
+    if (map[x] !== undefined) {
+      map[x] += Number(`${s}${k}`);
     } else {
-      console.log('Oversight in poly:', poly, 'character:', c, '@', i);
+      map[x] = Number(`${s}${k}`);
     }
   }
-  console.log('map',map);
 
-  var sort = {}
-  for (var Variable in map) {
-    var m = map[Variable]
-    if (sort[m]) {
-      sort[m] += Variable;
-    } else {
-      sort[m] = Variable;
-    }
-  }
-  console.log('sort', sort);
-
-  var simplified = '';
-  for (var Integer in sort) {
-    var Variables = sort[Integer];
-    if(Integer === '1') {
-      simplified += `+${Variables}`;
-    } else if (Integer === '-1') {
-      simplified += `-${Variables}`;
-    } else if (Integer === '0') {
-      continue;
-    } else {
-      var sign = Integer[0] === '-' ? '' : '+';
-      simplified += `${sign}${Integer}${Variables}`;
-    }
-  }
-  console.log('simplified', simplified);
+  simplified = '';
 
   return simplified.replace(/^\+/,'');
 }
